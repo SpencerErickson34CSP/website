a6cff0ec5ceded1bd606602a1ab8e2f1de49f30d
SpencerErickson34CSP <143059507+SpencerErickson34CSP@users.noreply.github.com>
2024-02-27 22:47:23 +0000

    Tue, Feb 27, 2024, 2:47 PM -08:00

--- js/loading.js
+++ js/loading.js
@@ -1,7 +1,18 @@
-
 progress = document.querySelector('.progress')
+bg = document.querySelector('.bg')
 
 load = 0
 
-progress.innerText = `${load}%`
-progress.style.opacity =
+int = setInterval(blurring, 50)
+
+function blurring()
+{
+    load = load + 1
+
+    if (load > 99)
+    {
+        clearInterval(int)
+    }
+    progress.innerText = `${load}%`
+    progress.style.opacity = 1
+}

ae675eab1938eca6798133f4692f8c215b7a9fff
SpencerErickson34CSP <143059507+SpencerErickson34CSP@users.noreply.github.com>
2024-02-29 22:40:25 +0000

    Thu, Feb 29, 2024, 2:40 PM -08:00

--- js/expanding.js
+++ js/expanding.js
@@ -2,13 +2,14 @@ panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
+        console.log("hello")
         removeActiveClasses()
         panel.classList.add('active')
     })
 })
 
 function removeActiveClasses(){
-    panels.forEach(panel) => {
+    panels.forEach(panel => {
         panel.classList.remove('active')
-    }
+    })
 }

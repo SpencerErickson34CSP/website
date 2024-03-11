c0429a2d79a3fdd900c8297aa4d10f0dbd0e061d
SpencerErickson34CSP <143059507+SpencerErickson34CSP@users.noreply.github.com>
2024-03-11 22:05:39 +0000

    Mon, Mar 11, 2024, 3:05 PM -07:00

--- js/landing.js
+++ js/landing.js
@@ -3,15 +3,21 @@ open = document.getElementById('open')
 modal = document.getElementById('modal')
 close = document.getElementById('close')
 
-
+// close nav bar
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
 })
-
+// open modal
 open.addEventListener('click', () => {
     modal.classList.add('show-modal')
 })
-
+// close modal with x
 close.addEventListener('click', () => {
     modal.classList.remove('show-modal')
+})
+// close modal w/outside click
+window.addEventListener('click', (e) => {
+    if (e.target == modal){
+        modal.classList.remove('show-modal')
+    }
 })
